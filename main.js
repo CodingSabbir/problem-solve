28e5986ee61bd73c7395c1f529b0cc7e23ed3c55
MD.Sabbir Rahman <mdsabbirrahmanm740@gmail.com>
2023-10-26 16:38:28 +0600

    reduce

--- main.js
+++ main.js
@@ -17,13 +17,22 @@ console.log(avgPercentage([700,650,847,365,320,]))
 
 
 
+// const maxMumNumber=(array)=>{
+//     let maxNum=array[0]
+// for(let num of array){
+//    if(num > maxNum){
+//      maxNum=num
+//    }
+// }
+// return maxNum
+// }
+// console.log(maxMumNumber([10,50,42,26,15,75,50,]))
+
 const maxMumNumber=(array)=>{
-    let maxNum=array[0]
-for(let num of array){
-   if(num > maxNum){
-     maxNum=num
-   }
-}
-return maxNum
+const result=array.reduce((acc,curr)=>{
+        const max= acc > curr ? acc : curr
+        return max
+    })
+    return result
 }
 console.log(maxMumNumber([10,50,42,26,15,75,50,]))
