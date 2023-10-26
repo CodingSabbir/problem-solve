427ad2b974da2c54b2a455a2218717995d6cf06b
MD.Sabbir Rahman <mdsabbirrahmanm740@gmail.com>
2023-10-26 16:10:24 +0600

    max number

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const avgPercentage=(arr)=>{
     
    for(let number of arr){
     if(number < 300 ){
-        return null;
+        return 'failed';
     }
    }
     
@@ -11,6 +11,19 @@ const avgPercentage=(arr)=>{
    const parcentens=aveNumber/1000
    const result=Math.round(parcentens*100)
    return result
-
 }
 console.log(avgPercentage([700,650,847,365,320,]))
+
+
+
+
+const maxMumNumber=(array)=>{
+    let maxNum=array[0]
+for(let num of array){
+   if(num > maxNum){
+     maxNum=num
+   }
+}
+return maxNum
+}
+console.log(maxMumNumber([10,50,42,26,15,75,50,]))
